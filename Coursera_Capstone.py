555c783048fb194de52f1a139d64ad4e4ad0963b
tumjay <55017177+tumjay@users.noreply.github.com>
2019-09-07 15:53:08 +0100

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# THE FOLLOWING NOTEBOOK WILL BE USED FOR THE COURSERA CAPSTONE PROJECT
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
